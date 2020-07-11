bc63bb46c6170409bd3e01636b45eddb8c592623
parth <parthshah8120@gmail.com>
2020-07-11 04:20:38 -0700

    Commit

---
+++ app/src/main/java/com/example/avertimed/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.avertimed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CountDownTimer countDownTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        countDownTimer = new CountDownTimer(2500, 500) {
+
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+
+                Intent intent=new Intent(MainActivity.this, SignUp_Activity.class);
+                if(getIntent().getExtras()!=null) {
+                    intent.putExtras(getIntent().getExtras());
+                    setIntent(null);
+                }
+
+                startActivity(intent);
+                overridePendingTransition(R.anim.in_from_right, R.anim.out_to_left);
+                finish();
+
+            }
+        }.start();
+
+    }
+
+}
